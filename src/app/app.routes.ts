99d8366caeafc141d9cec8358af9a3e8315c0626
w41k4z <alainricor@gmail.com>
2024-05-02 15:25:12 +0300

    Cart interface

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { CartComponent } from './pages/cart/cart.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Accueil',
+  },
+  {
+    path: 'cart',
+    component: CartComponent,
+    title: 'Panier',
   },
 ];
--- src/app/components/layout/nav-header/nav-header.component.ts
+++ src/app/components/layout/nav-header/nav-header.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './nav-header.component.html',
-  styleUrl: './nav-header.component.scss'
+  styleUrl: './nav-header.component.scss',
 })
-export class NavHeaderComponent {
-
-}
+export class NavHeaderComponent {}
