0fe07f6e36591d7c2a9561d6bf335bc79406b5fc
w41k4z <alainricor@gmail.com>
2024-05-02 16:48:14 +0300

    Login interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { NavHeaderComponent } from './components/layout/nav-header/nav-header.component';
 import { RouterModule } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavHeaderComponent, RouterModule],
+  imports: [RouterOutlet, NavHeaderComponent, RouterModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'homes';
+
+  constructor(private router: Router) {}
+
+  isLoginRoute() {
+    return this.router.url == '/login';
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,18 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CartComponent } from './pages/cart/cart.component';
 import { ProductDetailsComponent } from './pages/product-details/product-details.component';
+import { LoginComponent } from './pages/login/login.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     title: 'Accueil',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    title: 'Se connecter',
   },
   {
     path: 'cart',
