7255ac187d5745757fba409c13f9f70c54793bd1
To-Mamiarilaza <mamiarilaza.to@gmail.com>
2024-05-02 16:07:30 +0300

    Finished landing page

---
+++ src/app/pages/home/recent-product/recent-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recent-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './recent-product.component.html',
+  styleUrl: './recent-product.component.scss'
+})
+export class RecentProductComponent {
+
+}
